54e5aad475995a5b1eb73ee2f5fd0765994e7f53
PC <lucaslorran2016@gmail.com>
2024-10-29 16:00:42 -0300

    feat: adiciona score

--- js/script.js
+++ js/script.js
@@ -1,42 +1,41 @@
 const mandalinha = document.querySelector('.mandalinha');
 const pipe = document.querySelector('.pipe');
+const scoreDisplay = document.getElementById('score');
+let score = 0;
+let scored = false; 
 
 const jump = () => {
     mandalinha.classList.add('jump');
 
     setTimeout(() => {
-
         mandalinha.classList.remove('jump');
-
-        }, 500);
-}
+    }, 500);
+};
 
 const loop = setInterval(() => {
-
-    console.log('loop')
-
-
     const pipePosition = pipe.offsetLeft;
     const mandalinhaPosition = +window.getComputedStyle(mandalinha).bottom.replace('px', '');
 
-    console.log(mandalinhaPosition);
-
     if (pipePosition <= 120 && pipePosition > 0 && mandalinhaPosition < 80) {
-
+        // Colisão
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;   
-
         mandalinha.style.animation = 'none';
         mandalinha.style.bottom = `${mandalinhaPosition}px`;   
-
         mandalinha.src = 'images/game-over.png';
-        mandalinha.style.width = '210px'
-        mandalinha.style.marginLeft = '50px'
+        mandalinha.style.width = '210px';
+        mandalinha.style.marginLeft = '50px';
         mandalinha.style.zIndex = '10';
 
         clearInterval(loop);
+    } else if (pipePosition < 0 && !scored) {
+        score++;
+        scoreDisplay.textContent = score;
+        scored = true; 
+    } else if (pipePosition >= 120) {
+        
+        scored = false;
     }
-
 }, 10);
 
 document.addEventListener('keydown', jump);
