38d61e76d29cfa6b44e767f95766c1fa453c5715
PC <lucaslorran2016@gmail.com>
2024-10-30 11:56:45 -0300

    fix: Corrige nome do personagem

--- js/script.js
+++ js/script.js
@@ -1,31 +1,31 @@
-const mandalinha = document.querySelector('.mandalinha');
+const mandalinho = document.querySelector('.mandalinho');
 const pipe = document.querySelector('.pipe');
 const scoreDisplay = document.getElementById('score');
 let score = 0;
 let scored = false; 
 
 const jump = () => {
-    mandalinha.classList.add('jump');
+    mandalinho.classList.add('jump');
 
     setTimeout(() => {
-        mandalinha.classList.remove('jump');
+        mandalinho.classList.remove('jump');
     }, 500);
 };
 
 const loop = setInterval(() => {
     const pipePosition = pipe.offsetLeft;
-    const mandalinhaPosition = +window.getComputedStyle(mandalinha).bottom.replace('px', '');
+    const mandalinhoPosition = +window.getComputedStyle(mandalinho).bottom.replace('px', '');
 
-    if (pipePosition <= 120 && pipePosition > 0 && mandalinhaPosition < 80) {
+    if (pipePosition <= 120 && pipePosition > 0 && mandalinhoPosition < 80) {
         // Colisão
         pipe.style.animation = 'none';
         pipe.style.left = `${pipePosition}px`;   
-        mandalinha.style.animation = 'none';
-        mandalinha.style.bottom = `${mandalinhaPosition}px`;   
-        mandalinha.src = 'images/game-over.png';
-        mandalinha.style.width = '210px';
-        mandalinha.style.marginLeft = '50px';
-        mandalinha.style.zIndex = '10';
+        mandalinho.style.animation = 'none';
+        mandalinho.style.bottom = `${mandalinhoPosition}px`;   
+        mandalinho.src = 'images/game-over.png';
+        mandalinho.style.width = '210px';
+        mandalinho.style.marginLeft = '50px';
+        mandalinho.style.zIndex = '10';
 
         clearInterval(loop);
     } else if (pipePosition < 0 && !scored) {
